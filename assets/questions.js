ba054d5a8358fdd5baa16415cc421e3c16148cc6
Victoria Garner <75230731+MsMischief@users.noreply.github.com>
2021-01-22 21:08:40 -0800

    JavaScript File

---
+++ assets/questions.js
@@ -0,0 +1,12 @@
+var quiz = {
+    data: [
+    {
+      q : "What does DOM stand for?",
+      o : [
+        "Data Operational Map",
+        "Data Operations Management",
+        "Document Object Model",
+        "Data Object Model"
+      ],
+      a : 2 // arrays start with 0, so answer is 70 meters
+    },
