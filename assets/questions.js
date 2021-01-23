5813aa1622e0bc5c9d9a12f734968466a5cbc47a
Victoria Garner <75230731+MsMischief@users.noreply.github.com>
2021-01-22 21:12:55 -0800

    Added question #2

--- assets/questions.js
+++ assets/questions.js
@@ -10,3 +10,13 @@ var quiz = {
       ],
       a : 2 // arrays start with 0, so answer is 70 meters
     },
+    {
+        q : "What are Variables?",
+        o : [
+          "The easiest part of JavaScript",
+          "A mathematical symbol that produces a result based on two values",
+          "A way to package functionality",
+          "Containers that store values"
+        ],
+        a : 3
+    },
